db4b94768106266317d324529eef72c23c560bef
unknown <lehuutien27022002@gmail.com>
2024-01-29 22:28:45 +0700

    add and update product

--- models/Product.js
+++ models/Product.js
@@ -4,6 +4,8 @@ const ProductSchema = new Schema({
     title: { type: String, required: true },
     description: String,
     price: { type: Number, required: true }
+}, {
+    timestamps: true,
 });
 
 export const Product = models.Product || model('Product', ProductSchema);
