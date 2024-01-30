68ff5420b1a8bc41e9f0c1b96820c7bb09030f59
unknown <lehuutien27022002@gmail.com>
2024-01-30 10:16:09 +0700

    delete product and upload image using s3 aws

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
-export default function ProductForm({ _id, title: existingTitle, description: existingDescription, price: existingPrice }) {
+export default function ProductForm({ _id, title: existingTitle, description: existingDescription, price: existingPrice, images }) {
     const [title, setTitle] = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
     const [price, setPrice] = useState(existingPrice || '');
@@ -24,6 +24,18 @@ export default function ProductForm({ _id, title: existingTitle, description: ex
     if (goToProducts) {
         router.push('/products');
     }
+
+    async function uploadImages(ev) {
+        const files = ev.target?.files;
+        if (files?.length > 0) {
+            const data = new FormData();
+            for (const file of files) {
+                data.append('file', file);
+            }
+            const res = await axios.post('/api/upload', data);
+            console.log(res.data);
+        }
+    }
     return (
         <form onSubmit={saveProduct}>
             <label>Product name</label>
@@ -31,6 +43,21 @@ export default function ProductForm({ _id, title: existingTitle, description: ex
                 type="text"
                 placeholder="product name"
                 value={title} onChange={ev => setTitle(ev.target.value)} />
+
+            <label>Photos</label>
+            <div className="mb-2">
+                <label className="w-24 h-24 text-center cursor-pointer flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+                    </svg>
+                    <div>Upload</div>
+                    <input type="file" onChange={uploadImages} className="hidden" />
+                </label>
+                {!images?.length && (
+                    <div>No photos in this product</div>
+                )}
+            </div>
+
             <label>Description</label>
             <textarea
                 placeholder="description"
