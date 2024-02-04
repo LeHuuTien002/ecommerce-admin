9e76f311bfa0b2d1ce3bcd41ccfeb4e74bf35c16
unknown <lehuutien27022002@gmail.com>
2024-02-04 07:19:27 +0700

    save product images  with  db

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -2,16 +2,17 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
-export default function ProductForm({ _id, title: existingTitle, description: existingDescription, price: existingPrice, images }) {
+export default function ProductForm({ _id, title: existingTitle, description: existingDescription, price: existingPrice, images: existingImages }) {
     const [title, setTitle] = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
     const [price, setPrice] = useState(existingPrice || '');
+    const [images, setImages] = useState(existingImages || []);
     const [goToProducts, setgoToProducts] = useState(false);
     const router = useRouter();
 
     async function saveProduct(ev) {
         ev.preventDefault();
-        const data = { title, description, price };
+        const data = { title, description, price, images };
         if (_id) {
             //update
             await axios.put('/api/products', { ...data, _id });
@@ -33,7 +34,9 @@ export default function ProductForm({ _id, title: existingTitle, description: ex
                 data.append('file', file);
             }
             const res = await axios.post('/api/upload', data);
-            console.log(res.data);
+            setImages(oldImages => {
+                return [...oldImages, ...res.data.links];
+            })
         }
     }
     return (
@@ -45,7 +48,12 @@ export default function ProductForm({ _id, title: existingTitle, description: ex
                 value={title} onChange={ev => setTitle(ev.target.value)} />
 
             <label>Photos</label>
-            <div className="mb-2">
+            <div className="mb-2 flex flex-wrap gap-2">
+                {!!images?.length && images.map(link => (
+                    <div key={link} className="h-24">
+                        <img className=" rounded-lg" src={link} alt="" />
+                    </div>
+                ))}
                 <label className="w-24 h-24 text-center cursor-pointer flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
@@ -53,9 +61,6 @@ export default function ProductForm({ _id, title: existingTitle, description: ex
                     <div>Upload</div>
                     <input type="file" onChange={uploadImages} className="hidden" />
                 </label>
-                {!images?.length && (
-                    <div>No photos in this product</div>
-                )}
             </div>
 
             <label>Description</label>
--- models/Product.js
+++ models/Product.js
@@ -3,7 +3,8 @@ import { Schema, model, models } from "mongoose";
 const ProductSchema = new Schema({
     title: { type: String, required: true },
     description: String,
-    price: { type: Number, required: true }
+    price: { type: Number, required: true },
+    images: [{type: String}]
 }, {
     timestamps: true,
 });
